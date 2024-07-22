732176d6c4fa05ec83c60635db8ae6171e76b5f2
Youjiiin <yujinjang24@gmail.com>
2024-07-22 23:56:51 +0900

    reafactor: 카카오 api 분리

--- ondo_front/src/pages/map/Map.jsx
+++ ondo_front/src/pages/map/Map.jsx
@@ -6,6 +6,8 @@ import BottomModal from "../../components/modal/BottomModal";
 import Loading from "../../components/modal/Loading";
 import Near from "../../components/modal/Near";
 
+import { initializeMap, displayCenterInfo } from './KakaoAPI';
+
 import style from './Map.module.css';
 import search from '../../assets/search.png'
 import reload from '../../assets/reload.png';
@@ -24,76 +26,9 @@ export default function Map() {
     setRefresh(refresh * -1);
   }
 
-  // 지도 중심 좌표의 주소 정보를 표시하는 함수
-  const displayCenterInfo = (geocoder, coords) => {
-    geocoder.coord2RegionCode(coords.getLng(), coords.getLat(), (result, status) => {
-      if (status === kakao.maps.services.Status.OK) {
-        for (let i = 0; i < result.length; i++) {
-          if (result[i].region_type === 'H') {
-            setAddress(result[i].address_name);
-            break;
-          }
-        } 
-      }
-    });
-  };
-
-  // 클릭한 좌표의 상세 주소 정보를 검색하는 함수
-  const searchDetailAddrFromCoords = (geocoder, coords, marker, infowindow, mapInstance) => {
-    geocoder.coord2Address(coords.getLng(), coords.getLat(), (result, status) => {
-      if (status === kakao.maps.services.Status.OK) {
-        const roadAddr = result[0].road_address ? result[0].road_address.address_name : '';
-        let detailAddr = roadAddr ? `<div>도로명주소 : ${roadAddr}</div>` : '';
-        detailAddr += `<div>지번 주소 : ${result[0].address.address_name}</div>`;
-
-        const content = `<div>
-            <span>내 위치</span>
-            ${detailAddr}
-          </div>`;
-
-        // 클릭한 위치에 마커를 표시
-        marker.setPosition(coords);
-        marker.setMap(mapInstance);
-
-        // 클릭한 위치의 주소 정보를 인포윈도우에 표시
-        infowindow.setContent(content);
-        infowindow.open(mapInstance, marker);
-      }
-    });
-  };
-
   // 지도 초기화 및 이벤트 설정
   useEffect(() => {
-    const container = document.getElementById("map");
-    const options = {
-      center: new kakao.maps.LatLng(lat, lon),
-      level: 3,
-    };
-    const mapInstance = new kakao.maps.Map(container, options);
-    setMap(mapInstance);
-
-    const geocoderInstance = new kakao.maps.services.Geocoder();
-    setGeocoder(geocoderInstance);
-
-    const markerInstance = new kakao.maps.Marker();
-    setMarker(markerInstance);
-
-    const infowindowInstance = new kakao.maps.InfoWindow({ zindex: 1 });
-    setInfowindow(infowindowInstance);
-
-    // 지도 클릭 이벤트 등록
-    kakao.maps.event.addListener(mapInstance, 'click', function (mouseEvent) {
-      searchDetailAddrFromCoords(geocoderInstance, mouseEvent.latLng, markerInstance, infowindowInstance, mapInstance);
-    });
-
-    // 지도 중심 변경 이벤트 등록
-    kakao.maps.event.addListener(mapInstance, 'center_changed', function () {
-      displayCenterInfo(geocoderInstance, mapInstance.getCenter());
-    });
-
-    // 초기 중심 주소 정보 표시
-    displayCenterInfo(geocoderInstance, mapInstance.getCenter());
-
+    initializeMap("map", lat, lon, setMap, setGeocoder, setMarker, setInfowindow, setAddress);
   }, [refresh]);
 
   // 현재 위치 가져오기 및 지도 중심 이동
@@ -110,7 +45,7 @@ export default function Map() {
           map.setCenter(moveLatLon);
 
           // 현재 위치의 주소 정보 표시
-          displayCenterInfo(geocoder, moveLatLon);
+          displayCenterInfo(geocoder, moveLatLon, setAddress);
         });
       } else {
         alert('접속정보를 불러올 수 없어요!\n 권한을 체크해주세요!');
